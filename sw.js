10e39743641d0ae0c5b9ee9c8d189fda0e7b8670
kriptokon <kriptokonstantin@gmail.com>
2025-03-25 23:14:32 +0500

    Initial commit

--- sw.js
+++ sw.js
@@ -1,66 +1,32 @@
-const CACHE_NAME = 'bible-reading-tracker-v1';
+const CACHE_NAME = 'bible-reading-plan-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/styles.css',
-  '/js/app.js',
-  '/js/data.js',
-  '/js/theme.js',
-  '/icons/icon-72x72.png',
-  '/icons/icon-96x96.png',
-  '/icons/icon-128x128.png',
-  '/icons/icon-144x144.png',
-  '/icons/icon-152x152.png',
-  '/icons/icon-192x192.png',
-  '/icons/icon-384x384.png',
-  '/icons/icon-512x512.png',
+  './',
+  './index.html',
+  './css/styles.css',
+  './js/data.js',
+  './js/theme.js',
+  './js/app.js',
+  './icons/icon-192x192.png',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap',
   'https://unpkg.com/material-components-web@latest/dist/material-components-web.min.css',
   'https://unpkg.com/material-components-web@latest/dist/material-components-web.min.js'
 ];
 
-// Install Service Worker
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Listen for requests
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request)
-          .then((response) => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // Clone the response
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          });
-      })
+      .then(response => response || fetch(event.request))
   );
 });
 
-// Update Service Worker
 self.addEventListener('activate', (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
