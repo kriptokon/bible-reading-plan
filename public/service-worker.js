10a027f6c113b356223b312800534f0aaf028eb3
kriptokon <kriptokonstantin@gmail.com>
2025-03-25 23:47:13 +0500

    for android v2

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,14 @@
 const CACHE_NAME = 'todo-tracker-v1';
+const DYNAMIC_CACHE = 'dynamic-cache-v1';
+
 const urlsToCache = [
   '/',
   '/index.html',
   '/static/css/main.css',
-  '/static/js/main.js'
+  '/static/js/main.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -16,6 +21,26 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        // Возвращаем только кэшированные ресурсы
+        return response || new Response('Offline Only Mode', {
+          status: 200,
+          headers: new Headers({ 'Content-Type': 'text/plain' })
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
